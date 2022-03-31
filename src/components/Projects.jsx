39ae44b7308553313ff8a4cd54b0d4e6d9e5d9b4
fremen432 <cbmiller4897@gmail.com>
2022-03-31 16:45:44 -0500

    project card fade amimation working but not great

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,24 +7,18 @@ export default function Projects() {
         <div id="projects" className="section flex-around box">
 
             <h2 className="section-title">Projects</h2>
-
-            <div className="projectCards_container box ">
+            <div className="projectCards_container ">
 
             { projects.map( project => 
                 <>
-                    <div className="projectCard-wrapper" >
-                        <div id={'projectCard_' + project.projId} className='projectCard'>
-                            <img 
-                            className="projectCard-image " 
-                            src={project.image} 
-                            alt={'Image for Clayton Miller project ' + project.projName} 
-                            />
-                            <div className="projectCard-content">
-                                <p className='projectCard-name colorMain flex-center projTitle' >{project.projName}</p>
-                                <div className="projectLinks">
-                                    <a className="projectCard-link colorMain flex-center " href={project.liveLink} target="_blank">Live Page</a>
-                                    <a className="projectCard-link colorMain flex-center " href={project.projLink} target="_blank">Repository</a>
-                                </div>
+                    <div id={'projectCard_' + project.projId} className='projectCard'>
+                        <img className="projectCard-image " src={project.image} alt={'Image for Clayton Miller project ' + project.projName} />
+                        <div className="projectCard-fadeElement" ></div>
+                        <div className="projectCard-content">
+                            <p className='projectCard-name colorMain flex-center ' >{project.projName}</p>
+                            <div className="projectLinks">
+                                <a className="projectCard-link colorMain flex-center " href={project.liveLink} target="_blank">Live Page</a>
+                                <a className="projectCard-link colorMain flex-center " href={project.projLink} target="_blank">Repository</a>
                             </div>
                         </div>
                     </div>
