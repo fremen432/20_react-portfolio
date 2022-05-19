9ab8285013bb49a412f4a1e1a0242241b0109286
fremen432 <cbmiller4897@gmail.com>
2022-05-19 11:16:49 -0500

    changed link to restaurant website

--- src/data.js
+++ src/data.js
@@ -1,18 +1,22 @@
-import { AiOutlineMail, AiOutlineLinkedin, AiOutlineGithub } from 'react-icons/ai'
-import { BsStackOverflow } from 'react-icons/bs'
+import {
+	AiOutlineMail,
+	AiOutlineLinkedin,
+	AiOutlineGithub,
+} from "react-icons/ai";
+import { BsStackOverflow } from "react-icons/bs";
 
-import projectImage_burkholderEyelashes from './assets/Images/projectCard-Images/e-commerce.png'
-import projectImage_eazyEatz from './assets/Images/projectCard-Images/eazy-eatz.png'
-import projectImage_armadilloConstruction from './assets/Images/projectCard-Images/armadillo-materials.png'
-import projectImage_passwordGenerator from './assets/Images/projectCard-Images/passwordGenerator.png'
-import projectImage_workDayScheduler from './assets/Images/projectCard-Images/workDayScheduler.png'
-import projectImage_backEnd from './assets/Images/projectCard-Images/e-commerce-back-end.jpeg'
-import projectImage_teamProfile from './assets/Images/projectCard-Images/team-profile-generator.png'
+import projectImage_burkholderEyelashes from "./assets/Images/projectCard-Images/e-commerce.png";
+import projectImage_eazyEatz from "./assets/Images/projectCard-Images/eazy-eatz.png";
+import projectImage_armadilloConstruction from "./assets/Images/projectCard-Images/armadillo-materials.png";
+import projectImage_passwordGenerator from "./assets/Images/projectCard-Images/passwordGenerator.png";
+import projectImage_workDayScheduler from "./assets/Images/projectCard-Images/workDayScheduler.png";
+import projectImage_backEnd from "./assets/Images/projectCard-Images/e-commerce-back-end.jpeg";
+import projectImage_teamProfile from "./assets/Images/projectCard-Images/team-profile-generator.png";
 
-import projectImage_simplyPho_homepage from './assets/Images/projectCard-Images/simplyPho-homepage.png'
-import projectImage_simplyPho_orderpage from './assets/Images/projectCard-Images/simplyPho-orderpage.png'
-import projectImage_simplyPho_homepage_full from './assets/Images/projectCard-Images/simplyPho-homepage-full.png'
-import projectImage_simplyPho_orderpage_full from './assets/Images/projectCard-Images/simplyPho-orderpage-full.png'
+import projectImage_simplyPho_homepage from "./assets/Images/projectCard-Images/simplyPho-homepage.png";
+import projectImage_simplyPho_orderpage from "./assets/Images/projectCard-Images/simplyPho-orderpage.png";
+import projectImage_simplyPho_homepage_full from "./assets/Images/projectCard-Images/simplyPho-homepage-full.png";
+import projectImage_simplyPho_orderpage_full from "./assets/Images/projectCard-Images/simplyPho-orderpage-full.png";
 
 const iconSize = 30;
 
@@ -87,24 +91,20 @@ const projects = [
 ];
 
 const featuredProject = {
-    projName: 'Simply Pho House',
-    description: 'ReactJS Restaurant Website',
-    projLink: 'https://github.com/fremen432/online-restaraunt-order',
-    liveLink: 'https://adorable-tiramisu-8d772f.netlify.app/',
-    image: projectImage_simplyPho_homepage_full,
-    technologies: [
-        'React JS',
-        'Tailwind CSS',
-        'Javascript'
-    ]
-}
+	projName: "Simply Pho House",
+	description: "ReactJS Restaurant Website",
+	projLink: "https://github.com/fremen432/online-restaraunt-order",
+	liveLink: "https://adorable-tiramisu-8d772f.netlify.app/",
+	image: projectImage_simplyPho_homepage_full,
+	technologies: ["React JS", "Tailwind CSS", "Javascript"],
+};
 
 const topFour = [
 	{
 		projName: "Simply Pho House",
 		description: "ReactJS Restaurant Website",
 		projLink: "https://github.com/fremen432/online-restaraunt-order",
-		liveLink: "https://adorable-tiramisu-8d772f.netlify.app/",
+		liveLink: "https://restaurant-website-fremen432.netlify.app/",
 		image: projectImage_simplyPho_homepage_full,
 		technologies: ["React JS", "Tailwind CSS", "Javascript"],
 	},
@@ -137,26 +137,26 @@ const topFour = [
 ];
 
 const contactLinks = [
-    {
-        platform: 'Email',
-        href: 'mailto:cbmiller4897@gmail.com',
-        icon: <AiOutlineMail size={iconSize} />
-    },
-    {
-        platform: 'LinkedIn',
-        href: 'https://www.linkedin.com/in/clayton-miller-20844810a',
-        icon: <AiOutlineLinkedin size={iconSize} />
-    },
-    {
-        platform: 'GitHub',
-        href: 'https://github.com/fremen432',
-        icon: <AiOutlineGithub size={iconSize} />
-    },
-    // {
-    //     platform: 'Stack OverFlow',
-    //     href: 'https://stackoverflow.com/users/16523713/cmiller31',
-    //     icon: <BsStackOverflow size={iconSize} />
-    // }
-]
+	{
+		platform: "Email",
+		href: "mailto:cbmiller4897@gmail.com",
+		icon: <AiOutlineMail size={iconSize} />,
+	},
+	{
+		platform: "LinkedIn",
+		href: "https://www.linkedin.com/in/clayton-miller-20844810a",
+		icon: <AiOutlineLinkedin size={iconSize} />,
+	},
+	{
+		platform: "GitHub",
+		href: "https://github.com/fremen432",
+		icon: <AiOutlineGithub size={iconSize} />,
+	},
+	// {
+	//     platform: 'Stack OverFlow',
+	//     href: 'https://stackoverflow.com/users/16523713/cmiller31',
+	//     icon: <BsStackOverflow size={iconSize} />
+	// }
+];
 
-export { projects, featuredProject, contactLinks, topFour }
+export { projects, featuredProject, contactLinks, topFour };
